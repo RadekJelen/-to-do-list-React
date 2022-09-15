2aa264bb37adacbcb72854a1590474041dbe86f0
Radosław Jeleń <jelu140@gmail.com>
2022-09-15 17:09:07 +0200

    transfering of functionality to custom hook

--- src/App.js
+++ src/App.js
@@ -4,49 +4,19 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container"
-import { useState, useEffect } from "react";
+import { useTasks } from "./useTasks";
+import { useState } from "react";
+
 
 function App() {
   const [hideDoneTasks, setUpHideDone] = useState(false);
-  const storedTasks = localStorage.getItem("tasks");
-	const [tasks, setTasks] = useState(
-		storedTasks ? JSON.parse(storedTasks) : []
-	);
-
-	useEffect(() => {
-		localStorage.setItem("tasks", JSON.stringify(tasks));
-	}, [tasks]);
 
   const toggleHideDone = () => {
     setUpHideDone(hideDoneTasks => !hideDoneTasks);
   };
 
-  const deleteTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }));
-  };
-
-  const markAllTasksDone = () => {
-    setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (content) => {
-    setTasks(tasks => [...tasks, {
-      content,
-      done: false,
-      id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-    },
-    ]);
-  };
-
+  const { tasks, deleteTask, toggleTaskDone, markAllTasksDone, addNewTask } = useTasks();
+  
   return (
     <Container>
       <Header title="Lista zadań" />
